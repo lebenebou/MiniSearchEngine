e6850cd823ccc75d5ee1c17a566a37bb7567b840
Youssef Yammine <lebenbou@gmail.com>
2022-12-22 14:52:07 +0200

    added functions to get results in engine.py

--- engine.py
+++ engine.py
@@ -4,11 +4,34 @@ import os
 CURRENT_DIR = os.path.dirname(__file__)
 os.chdir(CURRENT_DIR)
 
-def item_results(query:str):
+def item_results(_dir: str, query: str):
 
-    for item in os.listdir():
-        print(item)
+    return [item for item in os.listdir(_dir)
+
+        if item.lower().startswith(query.lower())
+        and
+        (os.path.isfile(item) or os.path.isdir(item))
+    ]
+    # returns items that start with the query string and are either a file or folder
+
+def file_results(items: list[str]):
+
+    return [item for item in items
+
+        if os.path.isfile(item)
+    ]
+    # returns files results only
+
+def directory_results(items: list[str]):
+
+    return [item for item in items
+
+        if os.path.isdir(item)
+    ]
+    # returns directory results only
+
+
 
 if __name__=="__main__":
 
-    print(CURRENT_DIR)
+    print(directory_results(item_results(CURRENT_DIR, ".")))
